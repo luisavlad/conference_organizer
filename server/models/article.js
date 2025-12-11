6e6be3b864d87b83cec5802d7d11f0d896aace25
Luisa Vlad <luisa.vlad@lobyco.com>
2025-12-11 20:42:49 +0200

    added conference and article models

---
+++ server/models/article.js
@@ -0,0 +1,54 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../sequelize");
+
+const Article = sequelize.define(
+  "Article",
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+      unique: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      len: [1, 100],
+    },
+    summary: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      len: [1, 500],
+    },
+    pdfPath: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    status: {
+      type: DataTypes.ENUM('under_review', 'revision_required', 'accepted', 'rejected'),
+      defaultValue: 'under_review',
+      allowNull: false,
+    },
+    authorId: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+    },
+    conferenceId: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+    },
+    currentVersion: {
+      type: DataTypes.INTEGER,
+      defaultValue: 1,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = Article;
---
+++ server/models/conference.js
@@ -0,0 +1,60 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../sequelize");
+
+const Conference = sequelize.define(
+  "Conference",
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+      unique: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      len: [1, 100],
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      len: [1, 500],
+    },
+    location: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      len: [1, 100],
+    },
+    startDate: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+      allowNull: false,
+    },
+    endDate: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+      allowNull: false,
+    },
+    submissionStart: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+      allowNull: false,
+    },
+    submissionEnd: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+      allowNull: false,
+    },
+    createdBy: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = Conference;
